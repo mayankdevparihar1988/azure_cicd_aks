2bfe65ca7284b4a61b5e79b9ae7ad53ec5218701
mayankparihar <mayank.dev.parihar1988gmail.com>
2023-10-05 12:48:16 +0200

    adding iphone 15 pro

--- Shopping/Shopping.API/Data/ProductContext.cs
+++ Shopping/Shopping.API/Data/ProductContext.cs
@@ -34,6 +34,14 @@ namespace Shopping.API.Data
         {
             return new List<Product>()
             {
+                  new Product()
+                {
+                    Name = "IPhone 15 pro",
+                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    ImageFile = "product-1.png",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                },
                 new Product()
                 {
                     Name = "IPhone X",
